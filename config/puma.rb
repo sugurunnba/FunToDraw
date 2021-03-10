e967ec529fffcada97a1d6702385e5331081997d
sugurunnba <sugurunnba7@gmail.com>
2021-03-10 07:29:27 +0000

    docker setting

--- config/puma.rb
+++ config/puma.rb
@@ -38,16 +38,20 @@ plugin :tmp_restart
 
 # 本番環境構築のため記述
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-rails_root = Dir.pwd
-# 本番環境のみデーモン起動
-if Rails.env.production?
-  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
-  stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log'),
-    File.join(rails_root, 'log', 'puma-error.log'),
-    true
-  )
-  # デーモン
-  daemonize
-end
+
+# Docker導入のため記述
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+
+# rails_root = Dir.pwd
+# # 本番環境のみデーモン起動
+# if Rails.env.production?
+#   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+#   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+#   stdout_redirect(
+#     File.join(rails_root, 'log', 'puma.log'),
+#     File.join(rails_root, 'log', 'puma-error.log'),
+#     true
+#   )
+#   # デーモン
+#   daemonize
+# end
