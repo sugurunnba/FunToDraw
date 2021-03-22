398161de7c633e536b9c60c0a1c201e7becd7d9e
sugurunnba <sugurunnba7@gmail.com>
2021-03-22 12:48:49 +0000

    Fix => picture/question.show.html(Add contributor name)

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,8 @@
 class Picture < ApplicationRecord
-  
+
   has_many :picture_images, dependent: :destroy
   accepts_attachments_for :picture_images, attachment: :picture_image
-  
+
+  belongs_to :user
+
 end
