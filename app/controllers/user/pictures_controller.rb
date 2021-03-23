971cbc996f331909830f9ef66734a61169844ac3
sugurunnba <sugurunnba7@gmail.com>
2021-03-23 06:18:48 +0000

    Back => docker before building

--- app/controllers/user/pictures_controller.rb
+++ app/controllers/user/pictures_controller.rb
@@ -7,7 +7,7 @@ class User::PicturesController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
-    @picture.save!
+    @picture.save
     redirect_to pictures_path
   end
 
